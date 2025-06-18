02c40ff8dd239045bd43fb3fe23ede8b68fc7db8
kat0001 <katarzyna_michalska@icloud.com>
2025-06-18 13:04:07 +0200

    methods added

--- liczba_rzymska.py
+++ liczba_rzymska.py
@@ -10,3 +10,38 @@ class LiczbaRzymska:
         arab = str(self.liczba_arabska)
         arab = f"{arab:0>4}"
         return int(arab[-4]) * "M" + setki[int(arab[-3])] + dziesiatki[int(arab[-2])] + jednosci[int(arab[-1])]
+
+
+    def __int__(self):
+        return self.liczba_arabska
+
+    def __str__(self):
+        return f"{self.arabska_na_rzymska()} ({self.liczba_arabska})"
+
+    def __eq__(self, other):
+        return self.liczba_arabska == other.liczba_arabska
+
+    def __le__(self, other):
+        return self.liczba_arabska <= other.liczba_arabska
+
+    def __gt__(self, other):
+        return self.liczba_arabska > other.liczba_arabska
+
+    def __add__(self, other):
+        return LiczbaRzymska(self.liczba_arabska + other.liczba_arabska)
+
+    def __iadd__(self, other):
+        self.liczba_arabska += other.liczba_arabska
+        return self
+
+    def __contains__(self, litera):
+        return litera in self.arabska_na_rzymska()
+
+    def __len__(self):
+        return len(self.arabska_na_rzymska())
+
+    def __mul__(self, other):
+        return LiczbaRzymska(self.liczba_arabska * other.liczba_arabska)
+
+    def __sub__(self, other):
+        return LiczbaRzymska(self.liczba_arabska - other.liczba_arabska)
